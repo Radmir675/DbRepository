ce315cc73709f1f45027f0d260568fa6ca9563ec
Радмир Биктимиров <tembr.com@gmail.com>
2022-10-08 21:00:45 +0300

    Добавил консоль

--- DbRepository/Program.cs
+++ DbRepository/Program.cs
@@ -7,9 +7,7 @@ namespace DbRepository
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            ///
-            ///
-            ///
+            Console.WriteLine("Всем peace");
 
         }
     }
